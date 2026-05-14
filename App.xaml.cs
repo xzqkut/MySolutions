c9a4b7dfbe84ab18f2dc5543f352c357cae036b3
xzqkut <142806445+xzqkut@users.noreply.github.com>
2026-05-14 18:59:02 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using WpfApp4.Views;
+
+namespace WpfApp4;
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var loginWindow = new LoginWindow();
+        bool? loginResult = loginWindow.ShowDialog();
+
+        if (loginResult == true)
+        {
+            var mainWindow = new MainWindow();
+
+            MainWindow = mainWindow;
+            ShutdownMode = ShutdownMode.OnMainWindowClose;
+
+            mainWindow.Show();
+        }
+        else
+        {
+            Shutdown();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using WpfApp4.ViewModels;
+
+namespace WpfApp4;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var viewModel = new MainViewModel();
+        DataContext = viewModel;
+
+        Loaded += async (_, _) =>
+        {
+            try
+            {
+                await viewModel.LoadUsersAsync();
+            }
+            catch (Exception ex)
+            {
+                viewModel.StatusMessage = $"Ошибка загрузки пользователей: {ex.Message}";
+            }
+        };
+    }
+}
